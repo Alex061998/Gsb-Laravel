87a8775d9796627497e035f29fe3ae2ae55cd7fe
Alex061998 <71149835+Alex061998@users.noreply.github.com>
2021-11-02 11:33:53 +0100

    Add files via upload

---
+++ storage/framework/views/aea3744ff83d26a75495eb64fdff90f9a148d744.php
@@ -0,0 +1,12 @@
+
+<?php $__env->startSection('contenu1'); ?>
+	
+		<div id="contenu">
+				<?php echo e($nbFrais); ?> fiche(s) ont été détéctées pour <b><?php echo e($unVisiteur['nom'] . ' ' . $unVisiteur['prenom']); ?></b>. Voulez-vous le supprimer ?
+				</br>
+				<button><a href=" <?php echo e(route('chemin_validSup', $unVisiteur['id'])); ?> " >Valider</a></button>
+				<button><a href=" <?php echo e(route('chemin_afficherVisiteur')); ?> ">Annuler</a></button>
+		</div>
+<?php $__env->stopSection(); ?>
+ </form>
+<?php echo $__env->make('newrole', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\laragon\www\gsbLaravel\resources\views/check.blade.php ENDPATH**/ ?>
